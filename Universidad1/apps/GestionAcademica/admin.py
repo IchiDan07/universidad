f88e2743ad9137c074c327e147cef7bac4b23350
Ichida <corellaesp07@gmail.com>
2020-02-04 17:35:18 -0700

    Version 1

---
+++ Universidad1/apps/GestionAcademica/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from Universidad1.apps.GestionAcademica.models import *
+
+# Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(curso)
+admin.site.register(Matricula)
